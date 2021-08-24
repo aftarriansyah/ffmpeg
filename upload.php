0222a87c74f0a0126ab3d8b5bf7b05d8ad767185
aftarriansyah <64802313+aftarriansyah@users.noreply.github.com>
2021-08-25 03:17:21 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -16,16 +16,18 @@
   {
     $path = "uploads/";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
-
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+    
+    if($_FILES["file"]["type"] != "video/mp4"){
+      echo "only .mp4 file");
+    }elseif(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
       echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
       " has been uploaded {$_FILES["file"]["type"]}";
       $audioname = "audio_".md5(basename($_FILES['uploaded_file']['name'])).".mp4";
       $videoname = "video_".md5(basename($_FILES['uploaded_file']['name'])).".mp4";
       shell_exec("ffmpeg -i $path -c copy -an uploads/$videoname");
       shell_exec("ffmpeg -i $path -q:a 0 -map a uploads/$audioname");
-      echo "<a href="uploads/$audioname">$audioname</a><br>";
-       echo "<a href="uploads/$videoname">$videoname</a><br>";
+      echo "<a href=\"uploads/$audioname\">$audioname</a><br>";
+       echo "<a href=\"uploads/$videoname\">$videoname</a><br>";
       
     } else{
         echo "There was an error uploading the file, please try again!";
