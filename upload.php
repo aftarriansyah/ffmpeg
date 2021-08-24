9f00b388001d516a279864d3f303ceafedcb7909
aftarriansyah <64802313+aftarriansyah@users.noreply.github.com>
2021-08-25 03:15:49 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -22,8 +22,11 @@
       " has been uploaded {$_FILES["file"]["type"]}";
       $audioname = "audio_".md5(basename($_FILES['uploaded_file']['name'])).".mp4";
       $videoname = "video_".md5(basename($_FILES['uploaded_file']['name'])).".mp4";
-      shell_exec("ffmpeg -i $path -c copy -an uploads/$audioname");
-      echo $audioname;
+      shell_exec("ffmpeg -i $path -c copy -an uploads/$videoname");
+      shell_exec("ffmpeg -i $path -q:a 0 -map a uploads/$audioname");
+      echo "<a href="uploads/$audioname">$audioname</a><br>";
+       echo "<a href="uploads/$videoname">$videoname</a><br>";
+      
     } else{
         echo "There was an error uploading the file, please try again!";
     }
